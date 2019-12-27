e67bb103d1bea0852000c45fc4b2ffa8f57329ad
Ethan Choi <ethanchoi.812@gmail.com>
2019-12-27 16:03:37 +0800

    Add initial post submission feature

--- src/Editor.js
+++ src/Editor.js
@@ -1,14 +1,60 @@
 import React from 'react';
 
 class Editor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            content: ''
+         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        const post = {
+            title: this.state.title,
+            content: this.state.content
+        }
+
+        console.log(post);
+
+        this.setState({
+            title: '',
+            content: ''
+        });
+        
+        event.preventDefault();
+    }
+
     render(){
         return(
-            <div className="Editor">
+            <form className="Editor" onSubmit={this.handleSubmit} >
                 <button>+ New Post</button>
-                <input className="Title-editor" type="text" />
-                <textarea className="Content-editor"></textarea>
-                <button>Publish</button>
-            </div>
+                <input 
+                    name="title"
+                    className="Title-editor"
+                    type="text"
+                    value={this.state.title}
+                    onChange={this.handleChange} />
+                <textarea 
+                    name="content"
+                    className="Content-editor" 
+                    value={this.state.content} 
+                    onChange={this.handleChange} />
+                <input type="submit" value="Publish" />
+            </form>
         )
     }
 
